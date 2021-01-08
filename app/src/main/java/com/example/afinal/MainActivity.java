88f1221c4e407cf42041280a9f965b0e01996c81
Thea77 <limsokunthea111@gmail.com>
2021-01-08 16:56:57 +0700

    login fb

--- app/src/main/java/com/example/afinal/MainActivity.java
+++ app/src/main/java/com/example/afinal/MainActivity.java
@@ -1,26 +1,91 @@
 package com.example.afinal;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText email,password;
     Button login;
     TextView register;
+//    LinearLayout loginFB;
+
+    LoginButton loginButtonFB;
+    CallbackManager callbackManager;
+    FirebaseAuth mAuth;
+
+    String TAG = "Main";
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initUI();
+//        getHashKey();
+        callbackManager = CallbackManager.Factory.create();
+
+       loginButtonFB.setReadPermissions("email", "public_profile");
+       loginButtonFB.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+           @Override
+           public void onSuccess(LoginResult loginResult) {
+//               Toast.makeText(MainActivity.this,"Success",Toast.LENGTH_SHORT).show();
+               handleFacebookAccessToken(loginResult.getAccessToken());
+           }
+
+           @Override
+           public void onCancel() {
+
+           }
+
+           @Override
+           public void onError(FacebookException error) {
+
+           }
+       });
+
+
+
+
+
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,12 +97,55 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+       mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+           @Override
+           public void onComplete(@NonNull Task<AuthResult> task) {
+                FirebaseUser user = mAuth.getCurrentUser();
+
+                Log.d(TAG,"complete "+ user.getDisplayName());
+           }
+       });
+    }
+
     private void initUI() {
         email = findViewById(R.id.editText);
         password = findViewById(R.id.password);
         login = findViewById(R.id.login);
         register = findViewById(R.id.txtregister);
+        loginButtonFB = findViewById(R.id.loginButtonFB);
     }
 
+
+
+
+
+
+
+
+
+
+
+
+
+//get key hash
+
+
+//    private void getHashKey() {
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    getPackageName(),
+//                    PackageManager.GET_SIGNATURES);
+//            for(Signature signature : info.signatures){
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        }
+//        catch (PackageManager.NameNotFoundException | NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
 }
